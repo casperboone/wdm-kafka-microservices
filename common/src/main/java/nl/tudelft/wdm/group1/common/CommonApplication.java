01d360a521c51a15a95bb16b0654b14fcacf5be7
Georgios Andreadis <info@gandreadis.com>
2019-05-21 15:30:06 +0200

    Add main class

---
+++ common/src/main/java/nl/tudelft/wdm/group1/common/CommonApplication.java
@@ -0,0 +1,10 @@
+package nl.tudelft.wdm.group1.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+        System.out.println("Mock application");
+    }
+}
