6db1a7237bf5c04fad69e09a13e3bac950965712
RUKAIYIN <xintingapple@gmail.com>
2019-05-22 12:54:06 +0200

    Add order processed in stock topics
    Add `orderProcessedInStockSuccessful` and `orderProcessedInStockFailed`

--- common/src/main/java/nl/tudelft/wdm/group1/common/OrdersTopics.java
+++ common/src/main/java/nl/tudelft/wdm/group1/common/OrdersTopics.java
@@ -6,6 +6,8 @@ public class OrdersTopics {
     public static final String ORDER_DELETED = "orderDeleted";
     public static final String ORDER_ITEM_ADDED = "orderItemAdded";
     public static final String ORDER_ITEM_DELETED = "orderItemDeleted";
+    public static final String ORDER_PROCESSED_IN_STOCK_SUCC = "orderProcessedInStockSuccessful";
+    public static final String ORDER_PROCESSED_IN_STOCK_FAILED = "orderProcessedInStockFailed";
 
     public static String[] getTopics() {
         return new String[] {ORDER_CHECKED_OUT, ORDER_CREATED, ORDER_DELETED, ORDER_ITEM_ADDED, ORDER_ITEM_DELETED};
