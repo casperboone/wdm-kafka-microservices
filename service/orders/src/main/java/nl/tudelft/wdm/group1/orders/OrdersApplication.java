c21b78c60178263689d529e3d62825f6df99de88
Georgios Andreadis <info@gandreadis.com>
2019-05-28 12:08:00 +0200

    Implement persistence for orders

--- service/orders/src/main/java/nl/tudelft/wdm/group1/orders/OrdersApplication.java
+++ service/orders/src/main/java/nl/tudelft/wdm/group1/orders/OrdersApplication.java
@@ -2,8 +2,10 @@ package nl.tudelft.wdm.group1.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("nl.tudelft.wdm.group1.common")
 public class OrdersApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrdersApplication.class, args);
