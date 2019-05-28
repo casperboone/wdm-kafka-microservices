6529572b1c6de14e239aa0e8901d729a6cb76a4e
RUKAIYIN <xintingapple@gmail.com>
2019-05-28 11:29:47 +0200

    Add read/write lock to stock items

--- service/stock/src/main/java/nl/tudelft/wdm/group1/stock/events/Consumer.java
+++ service/stock/src/main/java/nl/tudelft/wdm/group1/stock/events/Consumer.java
@@ -1,16 +1,16 @@
 package nl.tudelft.wdm.group1.stock.events;
 
 import nl.tudelft.wdm.group1.common.*;
-import nl.tudelft.wdm.group1.stock.*;
 import nl.tudelft.wdm.group1.stock.StockItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Iterator;
 import java.util.UUID;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Service
 public class Consumer {
@@ -37,43 +37,57 @@ public class Consumer {
             throws ResourceNotFoundException, InsufficientStockException, InvalidStockChangeException {
         logger.info(String.format("#### -> Consumed message -> %s", order));
 
-        // TODO: lock the stock to prevent multiple checks on same stock
-        // check availability of all StockItems
-        for (UUID stockItemId : order.getItemIds()) {
-            StockItem stockItem;
-            try {
-                stockItem = stockItemRepository.find(stockItemId);
-            } catch (ResourceNotFoundException e) {
-                producer.emitStockItemsSubtractForOrderFailed(order);
-                return;
-            }
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        Lock readLock = readWriteLock.readLock();
+        Lock writeLock = readWriteLock.writeLock();
 
-            if (stockItem.getStock() < 1) { // amount per order item is 1
-                producer.emitStockItemsSubtractForOrderFailed(order);
-                return;
+        // lock all stock items
+        readLock.lock();
+        try {
+            // check availability of all StockItems
+            for (UUID stockItemId : order.getItemIds()) {
+                StockItem stockItem;
+                try {
+                    stockItem = stockItemRepository.find(stockItemId);
+                } catch (ResourceNotFoundException e) {
+                    producer.emitStockItemsSubtractForOrderFailed(order);
+                    return;
+                }
+
+                if (stockItem.getStock() < 1) { // amount per order item is 1
+                    producer.emitStockItemsSubtractForOrderFailed(order);
+                    return;
+                }
             }
+        } finally {
+            readLock.unlock();
         }
 
-        // all stocks available, proceed and emit a successful event
+        // all stock items available, proceed and emit a successful event
         int totalPrice = 0;
 
-        for (UUID stockItemId : order.getItemIds()) {
-            StockItem stockItem;
-            try {
-                stockItem = stockItemRepository.find(stockItemId);
-            } catch (ResourceNotFoundException e) {
-                // we assume this should not cause any exception
-                throw e;
-            }
+        writeLock.lock();
+        try {
+            for (UUID stockItemId : order.getItemIds()) {
+                StockItem stockItem;
+                try {
+                    stockItem = stockItemRepository.find(stockItemId);
+                } catch (ResourceNotFoundException e) {
+                    // we assume this should not cause any exception
+                    throw e;
+                }
 
-            try {
-                stockItem.subtractStock(1);
-            } catch (InsufficientStockException | InvalidStockChangeException e) {
-                // we assume the subtraction should not cause any exception
-                throw e;
-            }
+                try {
+                    stockItem.subtractStock(1);
+                } catch (InsufficientStockException | InvalidStockChangeException e) {
+                    // we assume the subtraction should not cause any exception
+                    throw e;
+                }
 
-            totalPrice += stockItem.getPrice();
+                totalPrice += stockItem.getPrice();
+            }
+        } finally {
+            writeLock.unlock();
         }
 
         order.setPrice(totalPrice);
