6aab5323007bb2ffd905a305f294ac81eaf33c2a
Gerben <g.j.w.oolbekkink@gmail.com>
2019-05-28 12:27:40 +0200

    Fix users call

--- service/users/src/test/java/nl/tudelft/wdm/group1/users/UsersApplicationTests.java
+++ service/users/src/test/java/nl/tudelft/wdm/group1/users/UsersApplicationTests.java
@@ -2,22 +2,40 @@ package nl.tudelft.wdm.group1.users;
 
 import com.jayway.jsonpath.JsonPath;
 import nl.tudelft.wdm.group1.common.CreditChangeInvalidException;
+import nl.tudelft.wdm.group1.common.KafkaResponse;
 import nl.tudelft.wdm.group1.common.ResourceNotFoundException;
+import nl.tudelft.wdm.group1.common.RestTopics;
 import nl.tudelft.wdm.group1.common.User;
+import nl.tudelft.wdm.group1.common.payload.*;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -50,6 +68,18 @@ public class UsersApplicationTests {
 
     private User defaultUser;
 
+    private static Consumer<String, KafkaResponse<User>> defaultConsumer;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("consumer" + UUID.randomUUID().toString(), "false", embeddedKafka.getEmbeddedKafka());
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        JsonDeserializer<KafkaResponse<User>> tJsonDeserializer = new JsonDeserializer<>();
+        tJsonDeserializer.addTrustedPackages("*");
+        defaultConsumer = new DefaultKafkaConsumerFactory<>(consumerProps, new StringDeserializer(), tJsonDeserializer).createConsumer();
+        defaultConsumer.subscribe(Collections.singletonList(RestTopics.RESPONSE));
+    }
+
     @Before
     public void setUp() throws CreditChangeInvalidException {
         defaultUser = new User("John", "Doe", "Mekelweg 4", "2628 CD", "Delft");
@@ -57,20 +87,20 @@ public class UsersApplicationTests {
         userRepository.add(defaultUser);
     }
 
+    private static <V> Producer<String, V> createProducer() {
+        Map<String, Object> senderProps = KafkaTestUtils.producerProps(embeddedKafka.getEmbeddedKafka());
+        return new DefaultKafkaProducerFactory<String, V>(senderProps, new StringSerializer(), new JsonSerializer<>()).createProducer();
+    }
+
     @Test
     public void createNewUser() throws Exception {
-        MvcResult result = this.mockMvc.perform(
-                post("/users")
-                        .param("firstName", "Jane")
-                        .param("lastName", "Da")
-                        .param("street", "Main Street")
-                        .param("zip", "90101")
-                        .param("city", "Rome")
-        ).andExpect(status().isOk()).andReturn();
+        createProducer().send(new ProducerRecord<>(RestTopics.REQUEST, "", new UserCreatePayload("Jane", "Da", "Main Street", "90101", "Rome")));
+        ConsumerRecord<String, KafkaResponse<User>> userRecord = KafkaTestUtils.getSingleRecord(defaultConsumer, RestTopics.RESPONSE);
+        User result = userRecord.value().getPayload();
 
-        await().until(() -> userRepository.contains(UUID.fromString(getJsonValue(result, "$.id"))));
+        await().until(() -> userRepository.contains(result.getId()));
 
-        User user = userRepository.find(UUID.fromString(getJsonValue(result, "$.id")));
+        User user = userRepository.find(result.getId());
 
         assertThat(user.getFirstName()).isEqualTo("Jane");
         assertThat(user.getLastName()).isEqualTo("Da");
@@ -81,77 +111,65 @@ public class UsersApplicationTests {
     }
 
     @Test
-    public void retrieveAUser() throws Exception {
-        this.mockMvc.perform(get("/users/" + defaultUser.getId()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(defaultUser.getId().toString())))
-                .andExpect(jsonPath("$.firstName", is("John")))
-                .andExpect(jsonPath("$.lastName", is("Doe")))
-                .andExpect(jsonPath("$.street", is("Mekelweg 4")))
-                .andExpect(jsonPath("$.zip", is("2628 CD")))
-                .andExpect(jsonPath("$.city", is("Delft")))
-                .andExpect(jsonPath("$.credit", is(2249)));
+    public void retrieveAUser() {
+        createProducer().send(new ProducerRecord<>(RestTopics.REQUEST, "", new UserGetPayload(defaultUser.getId())));
+        ConsumerRecord<String, KafkaResponse<User>> userRecord = KafkaTestUtils.getSingleRecord(defaultConsumer, RestTopics.RESPONSE);
+        User user = userRecord.value().getPayload();
+
+        assertThat(user.getFirstName()).isEqualTo("John");
+        assertThat(user.getLastName()).isEqualTo("Doe");
+        assertThat(user.getStreet()).isEqualTo("Mekelweg 4");
+        assertThat(user.getZip()).isEqualTo("2628 CD");
+        assertThat(user.getCity()).isEqualTo("Delft");
+        assertThat(user.getCredit()).isEqualTo(2249);
     }
 
     @Test
-    public void removeAUser() throws Exception {
-        this.mockMvc.perform(delete("/users/" + defaultUser.getId()))
-                .andExpect(status().isOk());
+    public void removeAUser() {
+        createProducer().send(new ProducerRecord<>(RestTopics.REQUEST, "", new UserDeletePayload(defaultUser.getId())));
+        KafkaTestUtils.getSingleRecord(defaultConsumer, RestTopics.RESPONSE);
 
         await().untilAsserted(() -> assertThatThrownBy(() -> userRepository.find(defaultUser.getId()))
                 .isInstanceOf(ResourceNotFoundException.class));
     }
 
     @Test
-    public void retrieveCredit() throws Exception {
-        this.mockMvc.perform(get("/users/" + defaultUser.getId()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.credit", is(2249)));
-    }
+    public void addCredit() {
+        createProducer().send(new ProducerRecord<>(RestTopics.REQUEST, "", new UserCreditAddPayload(defaultUser.getId(), 1500)));
+        KafkaTestUtils.getSingleRecord(defaultConsumer, RestTopics.RESPONSE);
 
-    @Test
-    public void addCredit() throws Exception {
-        this.mockMvc.perform(post("/users/" + defaultUser.getId() + "/credit/add/1500"))
-                .andExpect(status().isOk());
-
-        assertThat(defaultUser.getCredit()).isEqualTo(3749);
+        await().untilAsserted(() -> assertThat(defaultUser.getCredit()).isEqualTo(3749));
     }
 
     @Test
     public void addNegativeCreditAmount() throws Exception {
-        this.mockMvc.perform(post("/users/" + defaultUser.getId() + "/credit/add/-1500"))
-                .andExpect(status().isUnprocessableEntity());
+        createProducer().send(new ProducerRecord<>(RestTopics.REQUEST, "", new UserCreditAddPayload(defaultUser.getId(), -1500)));
+        KafkaTestUtils.getSingleRecord(defaultConsumer, RestTopics.RESPONSE);
 
         assertThat(defaultUser.getCredit()).isEqualTo(2249);
     }
 
     @Test
     public void subtractCreditWhenAvailable() throws Exception {
-        this.mockMvc.perform(post("/users/" + defaultUser.getId() + "/credit/subtract/1500"))
-                .andExpect(status().isOk());
+        createProducer().send(new ProducerRecord<>(RestTopics.REQUEST, "", new UserCreditSubtractPayload(defaultUser.getId(), 1500)));
+        KafkaTestUtils.getSingleRecord(defaultConsumer, RestTopics.RESPONSE);
 
         assertThat(defaultUser.getCredit()).isEqualTo(749);
     }
 
     @Test
     public void subtractCreditWhenNotAvailable() throws Exception {
-        this.mockMvc.perform(post("/users/" + defaultUser.getId() + "/credit/subtract/3000"))
-                .andExpect(status().isUnprocessableEntity());
+        createProducer().send(new ProducerRecord<>(RestTopics.REQUEST, "", new UserCreditSubtractPayload(defaultUser.getId(), 3000)));
+        KafkaTestUtils.getSingleRecord(defaultConsumer, RestTopics.RESPONSE);
 
         assertThat(defaultUser.getCredit()).isEqualTo(2249);
     }
 
     @Test
     public void subtractNegativeCreditAmount() throws Exception {
-        this.mockMvc.perform(post("/users/" + defaultUser.getId() + "/credit/subtract/-1500"))
-                .andExpect(status().isUnprocessableEntity());
+        createProducer().send(new ProducerRecord<>(RestTopics.REQUEST, "", new UserCreditSubtractPayload(defaultUser.getId(), -1500)));
+        KafkaTestUtils.getSingleRecord(defaultConsumer, RestTopics.RESPONSE);
 
         assertThat(defaultUser.getCredit()).isEqualTo(2249);
-    }
-
-    private String getJsonValue(MvcResult mvcResult, String path) throws UnsupportedEncodingException {
-        String response = mvcResult.getResponse().getContentAsString();
-
-        return JsonPath.parse(response).read(path).toString();
     }
 }
